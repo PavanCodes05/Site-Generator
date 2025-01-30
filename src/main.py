15fd17531438cadb30903d8e99e4e831b23e8235
PavanCodes05 <pavancodes05@gmail.com>
2025-01-30 20:28:53 +0530

    Add Tests-TextNode

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from textnode import TextType, TextNode
 
 def main():
-    dummy = TextNode("This is a text node",TextType.BOLD, "https://boot.dev")
+    dummy = TextNode("This is a text node", TextType.BOLD, "https://boot.dev")
     print(dummy)
 
 main()
