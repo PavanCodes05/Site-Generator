c099168350f87ad39da9b79fc7f2a3e3fdfb6958
PavanCodes05 <pavancodes05@gmail.com>
2025-01-30 20:08:22 +0530

    Create TextNode

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
-print("Hello world!")
+from textnode import TextType, TextNode
+
+def main():
+    dummy = TextNode("This is a text node",TextType.BOLD, "https://boot.dev")
+    print(dummy)
+
+main()
--- src/textnode.py
+++ src/textnode.py
@@ -7,3 +7,17 @@ class TextType(Enum):
     CODE = "code"
     LINKS = "links"
     IMAGES = "images"
+
+class TextNode:
+    def __init__(self, text, text_type:TextType, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+    
+    def __eg__(self, obj1, obj2):
+        return (obj1.text == obj2.text and
+                obj1.text_type == obj2.text_type and 
+                obj1.url == obj2.url)
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
