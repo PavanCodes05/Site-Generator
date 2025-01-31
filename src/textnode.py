b34c3eaff33c97816f089f0febf7f7a6340964d5
PavanCodes05 <pavancodes05@gmail.com>
2025-01-31 19:36:22 +0530

    Add Text-HTML

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
     TEXT = "text"
@@ -21,3 +22,20 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    
+    def text_node_to_html_node(self, text_node):
+        if not isinstance(self.text_type, TextNode):
+            raise ValueError("Not valid type")
+        
+        if self.text_type == TextType.TEXT:
+            return LeafNode(None, text_node.text)
+        elif self.text_type == TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        elif self.text_type == TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        elif self.text_type == TextType.CODE:
+            return LeafNode("code", text_node.text)
+        elif self.text_type == TextType.IMAGE:
+            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+        elif self.text_type == TextType.LINKS:
+            return LeafNode("a", text_node.text, {"href": text_node.url})
